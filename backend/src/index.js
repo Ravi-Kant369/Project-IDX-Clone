786f63a993181366073139e42e65883c07198a2b
Ravi Kant <ravikant2303@gmail.com>
2025-01-22 09:39:56 +0530

    Added code for created a new project on the backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,15 @@ import express from 'express'
 import { PORT } from './config/serverConfig.js';
 import cors from 'cors'
 const app = express();
+import apiRouter from './routes/index.js'
 
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+app.use('/api',apiRouter);
 
 app.get('/ping',(req,res) =>{
    return res.json({message:'pong'});
